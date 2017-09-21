95252305de3b32f8033de2f4d2111706c52fd62d
Alexander Hirschfeld <alex@d-qoi.com>
2017-09-21 01:24:19 +0000

    Done

--- hw03/etch-a-sketch-8x8.py
+++ hw03/etch-a-sketch-8x8.py
@@ -1,3 +1,5 @@
+#! /usr/bin/python3
+
 import smbus
 from time import time, sleep
 
--- hw03/tempReader.sh
+++ hw03/tempReader.sh
@@ -1,22 +1,68 @@
 #! /bin/bash
 
+greet() {
+    echo "Welcome to the temp detector!"
+    echo "This will poll a tmp101 sensor's alert pin, attached to GP1_4"
+    echo "If a value (in celcius) is passed to this function, it will set that as the alarm value, otherwise it will default to 20c"
+}
+
+setup() {
+    echo 97 > /sys/class/gpio/export
+    i2cset -y 1 "$1" 0x1 0x00
+    i2cset -y 1 "$1" 0x2 "0x$2"
+    i2cset -y 1 "$1" 0x3 "0x$2"
+}
+
+desetup() {
+    echo 97 > /sys/class/gpio/unexport
+    exit 0
+}
+
+trap desetup INT
+
+SENSOR=0x48
+TEMP=20
+DEBUG=0
+
+while getopts :t:s:hd opt; do
+    case $opt in
+        t)
+        echo "Temp set to $OPTARG" >&2
+        TEMP=$OPTARG
+        ;;
+        s)
+        echo "Sensor set to $OPTARG" >&2
+        SENSOR=$OPTARG
+        ;;
+        h)
+        echo "Use -s <sensor(hex)> and -t <temp(c)> to pass target temp and sensor, -d is debug mode" >&2
+        exit 1
+        ;;
+        d)
+        DEBUG=1
+        ;;
+        \?)
+        echo "Invalid Argument: -$OPTARG" >&2 
+        exit 1
+        ;;
+    esac
+done
+
+TEMP=$(echo "obase=16; $TEMP" | bc)
+greet
+setup $SENSOR $TEMP
 
 while true; do
-    TMP1=$(printf "%d" $(i2cget -y 1 0x45 1 w))
-    TMP2=$(printf "%d" `i2cget -y 1 0x48 0`)
-    TMP3=$(printf "%d" `i2cget -y 1 0x49 0`)
+
+    PINVAL=$(cat /sys/class/gpio/gpio97/value)
     
-    ALARM=""
-    if [ "$TMP1" -gt "13" ]; then
-        ALARM="TMP1 > 23"
+    if [ $PINVAL -eq 0 ]; then
+        echo "Alert!" 
+        echo "Temp = $(echo $(printf "%d*9/5+32" $(i2cget -y 1 $SENSOR 0)) | bc)°f"
     fi
-    if [ $TMP2 -gt "23" ]; then
-        ALARM="$ALARM TMP2 > 23"
+    if [ $DEBUG -eq 1 ]; then
+        echo "TEMP = 0x$TEMP, READ = $(i2cget -y 1 $SENSOR 0)"
     fi
-    if [ $TMP3 -gt "23" ]; then
-        ALARM="$ALARM TMP3 > 23"
-    fi
-    echo $ALARM
-    echo "TMP1: $TMP1, TMP2: $TMP2, TMP3: $TMP3"
     sleep 1
 done
+
