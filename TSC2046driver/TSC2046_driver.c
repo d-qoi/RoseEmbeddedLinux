3f4173f74002cbe92de8d2f9e52826533d561d60
Alexander Hirschfeld <alex@d-qoi.com>
2017-11-09 18:30:33 +0000

    Need to fix a lot of bugs, but start of workqueue impl

--- TSC2046driver/TSC2046_driver.c
+++ TSC2046driver/TSC2046_driver.c
@@ -52,6 +52,7 @@
 #include <linux/sysfs.h>
 #include <linux/gpio.h>
 #include <linux/workqueue.h>
+#include <linux/time.h>
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Alex Hirschfeld");
@@ -64,15 +65,22 @@ MODULE_VERSION("0.1");
 #define DEVICE_NAME "TSC2046"
 #define SPI_BUS_SPEED 2000000 //2MHz
 
-static struct spi_device *spi_dev;
+#define CREATE_COMMAND(ADC) ((1<<7 | (ADC & 0x7)<<4 | (mode & 0x1)<<3 | (SER & 0x1)<<2 |(device_obj->PD_vals & 0x3));)
 
-static struct workqueue_struct* tsc2046_workqueue;
+static struct spi_device *spi_dev;
 
 static short penirq;
 static short spiport;
 static short spics;
+static short mode;
+static short SER;
 
 static unsigned int irqNumber; 
+
+static struct workqueue_struct* wq;
+static struct work_struct main_spi_work_struct;
+static struct work_struct running_work_struct;
+static struct work_struct oneshot_work_struct;
 
 struct screenVals {
     int x;
@@ -80,14 +88,21 @@ struct screenVals {
     int z1;
     int z2;
     bool touch;
+    unsigned char adc[4] = {0b001, 0b101, 0b011, 0b100};
+    unsigned char i = 0;
 };
 
 struct diffVals {
-    unsigned int time_down;
+    int start_x;
+    int start_y;
+    int start_z1;
+    int start_z2;
+    long long start_time_ns;
     int diff_x;
     int diff_y;
     int diff_z1;
     int diff_z2;
+    long long time_down_ns;
 };
 
 struct oneshotVals {
@@ -100,16 +115,13 @@ struct runningVals {
     unsigned char data[2];
 };
 
-// Module Params
-module_param(spics, short, S_IRUSR | S_IRGRP | S_IROTH);
-MODULE_PARM_DESC(spics, "Chip Select");
-module_param(spiport, short, S_IRUSR | S_IRGRP | S_IROTH);
-MODULE_PARM_DESC(spiport, "SPI port");
-module_param(penirq, short, S_IRUSR | S_IRGRP | S_IROTH);
-MODULE_PARM_DESC(penirq, "Pen Interrupt Port");
-
 // Interrupts
 static irq_handler_t penirq_interrupt_handler(unsigned int irq, void* dev_id, struct pt_regs *reg);
+
+//helper function
+long long get_time_ns (void)
+{ return timespec_to_ns(current_kernel_time()); }
+
 
 // Kobject and sysfs
 static struct kset *TSC2046_kset;
@@ -135,8 +147,10 @@ struct TSC2046_attr {
 #define to_TSC2046_attr(x) container_of(x, struct TSC2046_attr, attr);
 
 static ssize_t TSC2046_vals_show(struct TSC2046_obj* obj, struct TSC2046_attr* attr, char* buf) {
-    printk(KERN_INFO "TSC2046: Vals Show called");
-    struct screenVals* vals = &obj->vals;
+    struct screenVals* vals;
+    vals = &obj->vals;
+    
+    printk(KERN_INFO "TSC2046: Vals Show called\n");
     return sprintf(buf, "x\t%d\ny\t%d\nz1\t%d\nz2\t%d\nactive\t%d\n",
         vals->x,
         vals->y,
@@ -146,10 +160,20 @@ static ssize_t TSC2046_vals_show(struct TSC2046_obj* obj, struct TSC2046_attr* a
 }
 
 static ssize_t TSC2046_diffs_show(struct TSC2046_obj* obj, struct TSC2046_attr* attr, char* buf) {
-    return 0;
+    struct diffVals* vals;
+    vals = &obj->diffs;
+    
+    printk(KERN_INFO "TSC2046: diffs show called\n");
+    return sprintf(buf, "x\t%d\ny\t%d\nz1\t%d\nz2\t%d\ntime\t%lld\n",
+        vals->diff_x,
+        vals->diff_y,
+        vals->diff_z1,
+        vals->diff_z2,
+        (vals->time_down_ns) ? vals->time_down_ns : (get_time_ns() - vals->start_time_ns));
 }
 
 static ssize_t TSC2046_batt_show(struct TSC2046_obj* obj, struct TSC2046_attr* attr, char* buf) {
+    printk(KERN_INFO "TSC2046: batt show called\n");
     return 0;
 }
 
@@ -166,11 +190,29 @@ static ssize_t TSC2046_PD_store(struct TSC2046_obj* obj, struct TSC2046_attr* at
 }
 
 static ssize_t TSC2046_oneshot_show(struct TSC2046_obj* obj, struct TSC2046_attr* attr, char* buf) {
-    return 0;
+    struct oneshotVals* vals;
+    vals = &obj->oneshot;
+    
+    printk(KERN_INFO "TSC2046: Oneshow Show Called\n");
+    return sprintf(buf, "command\t0x%02x\ndata\t0x%04x\n",
+        (unsigned int)vals->command,
+        (unsigned int)(vals->data[0]<<8 | vals->data[1]));
 }
 
 static ssize_t TSC2046_oneshot_store(struct TSC2046_obj* obj, struct TSC2046_attr* attr, const char* buf, size_t count) {
-    return 0;
+    struct oneshotVals* vals;
+    unsigned int adc_val;
+    
+    printk(KERN_INFO "TSC2046: Oneshot store called\n");
+    printk(KERN_INFO "TSC2046: count: %d, buf: %s", count, buf);
+    if (count != 2) {
+        return -1;
+    }
+    sscanf(buf, "%x", &adc_val);
+    printk(KERN_INFO "TSC2046: %d", adc_val);
+    vals->command = CREATE_COMMAND(adc_val);
+    printk(KERN_INFO "TSC2046: command %x\n", vals->command);
+    return count;
 }
 
 static void TSC2046_release(struct kobject* kobj) {
@@ -180,17 +222,18 @@ static void TSC2046_release(struct kobject* kobj) {
 }
 
 static struct TSC2046_attr vals_attr = 
-    __ATTR(vals, 0440, TSC2046_vals_show, NULL);
+    __ATTR(vals, S_IRUSR | S_IRGRP | S_IROTH, TSC2046_vals_show, NULL);
 static struct TSC2046_attr diffs_attr = 
-    __ATTR(diffs, 0440, TSC2046_diffs_show, NULL);
+    __ATTR(diffs, S_IRUSR | S_IRGRP | S_IROTH, TSC2046_diffs_show, NULL);
 static struct TSC2046_attr batt_attr = 
-    __ATTR(battery, 0440, TSC2046_batt_show, NULL);
+    __ATTR(battery, S_IRUSR | S_IRGRP | S_IROTH, TSC2046_batt_show, NULL);
 static struct TSC2046_attr temp_attr = 
-    __ATTR(temperature, 0440, TSC2046_temp_show, NULL);
+    __ATTR(temperature, S_IRUSR | S_IRGRP | S_IROTH, TSC2046_temp_show, NULL);
+    // It is a bad idea to make sys files writable to all, using 664 for these, sorry
 static struct TSC2046_attr PD_attr = 
-    __ATTR(PD_select, 0660, TSC2046_PD_show, TSC2046_PD_store);
+    __ATTR(PD_select, S_IWUSR | S_IRUSR | S_IRGRP | S_IWGRP | S_IROTH, TSC2046_PD_show, TSC2046_PD_store);
 static struct TSC2046_attr oneshot_attr = 
-    __ATTR(oneshot, 0660, TSC2046_oneshot_show, TSC2046_oneshot_store);
+    __ATTR(oneshot, S_IWUSR | S_IRUSR | S_IRGRP | S_IWGRP | S_IROTH, TSC2046_oneshot_show, TSC2046_oneshot_store);
     
 static struct attribute *TSC2046_default_attrs[] = {
     &vals_attr.attr,
@@ -262,6 +305,74 @@ static void destroy_TSC2046_obj(struct TSC2046_obj* obj) {
     kobject_put(&obj->kobj);
 }
 
+
+// Module Params
+module_param(spics, short, S_IRUSR | S_IRGRP | S_IROTH);
+MODULE_PARM_DESC(spics, "Chip Select");
+module_param(spiport, short, S_IRUSR | S_IRGRP | S_IROTH);
+MODULE_PARM_DESC(spiport, "SPI port");
+module_param(penirq, short, S_IRUSR | S_IRGRP | S_IROTH);
+MODULE_PARM_DESC(penirq, "Pen Interrupt Port");
+module_param(mode, short, S_IRUSR | S_IRGRP | S_IROTH);
+MODULE_PARM_DESC(mode, "8 or 12 bit mode");
+module_param(SER, short, S_IRUSR | S_IRGRP | S_IROTH);
+MODULE_PARM_DESC(SER, "single or dual channel mode");
+
+
+// Work Queue Stuff
+static void main_spi_work_handler(struct work_struct* data) {
+    struct screenVals* vals;
+    struct diffVals* diffs;
+    unsigned char command;
+    unsigned char data[2];
+    printk(KERN_INFO "TSC2046: Main Work Handler Called\n");
+    vals = &device_obj->vals;
+    diffs = &device_obj->diffs;
+    if (vals->touch) {
+        command = CREATE_COMMAND(vals->adc[vals->i]);
+        spi_write_then_read(spi_dev, (void*)&command, 1, (void*)&data, 2)
+        switch(vals->i) {
+            case 0:
+                vals->y = (int)(data[0]<<4 | data[0]>>4);
+                if(!diffs->start_y)
+                    diffs->start_y = vals->y;
+                diffs->diff_y = vals->y - diffs->start_y
+                break;
+            case 1:
+                vals->x = (int)(data[0]<<4 | data[0]>>4);
+                if(!diffs->start_x)
+                    diffs->start_x = vals->x;
+                diffs->diff_x = vals->x - diffs->start_x;
+                break;
+            case 2:
+                vals->z1 = (int)(data[0]<<4 | data[0]>>4);
+                if(!diffs->start_z1)
+                    diffs->start_z1 = vals->z1;
+                diffs->diff_z1 = vals->z1 - diffs->start_z1;
+                break;
+            case 3:
+                vals->z2 = (int)(data[0]<<4 | data[0]>>4);
+                if(!diffs->start_z2)
+                    diffs->start_z2 = vals->z2;
+                diffs->diff_z2 = vals->z2 - diffs->start_z2;
+                break;
+            default:
+                vals->i = -1;
+        }
+        vals->i = (vals->i + 1)%4;
+        schedule_work(&work);
+    } else {
+        
+    }
+}
+static void running_work_handler(struct work_struct* data) {
+    
+}
+static void oneshot_work_handler(struct work_struct* data) {
+    
+}
+
+
 // Initializations
 static int __init initialization(void){
     int status;
@@ -272,19 +383,25 @@ static int __init initialization(void){
     
     printk(KERN_INFO "TSC2046: Initializing\n");
     
-    printk(KERN_INFO "TSC2046: Params :: spics: %hd :: spiport:%hd :: penirq: %hd\n", spics, spiport, penirq);
-    
+    printk(KERN_INFO "TSC2046: Params :: spics: %hd :: spiport:%hd :: penirq: %hd :: mode: %hd :: SER: %hd\n", spics, spiport, penirq, mode, SER);
     
     if (spics == 0 && spiport == 0 && penirq == 0) {
         printk(KERN_INFO "TSC2046: Invalid params, please values for spics, spiport, and penirq\n");
         return -EINVAL;
     }
-    //printk(KERN_INFO "TS2046: Passed params\n");
+    
+    wq = create_singlethread_workqueue("TSC2046 Work Queue");
+    if (!wq) {
+        destroy_workqueue(wq);
+        printk(KERN_INFO "TSC2046: Unable to create workqueue");
+        return -EINVAL;
+    }
     
     TSC2046_kset = kset_create_and_add(DRIVER_NAME, NULL, firmware_kobj);
     if (!TSC2046_kset) {
         kset_unregister(TSC2046_kset);
-        printk(KERN_INFO "TSC2046: Unable to register kset in firmware");
+        printk(KERN_INFO "TSC2046: Unable to register kset in firmware\n");
+        destroy_workqueue(wq);
         return -EINVAL;
     }
     
@@ -292,7 +409,8 @@ static int __init initialization(void){
     if (!device_obj) {
         destroy_TSC2046_obj(device_obj);
         kset_unregister(TSC2046_kset);
-        printk(KERN_INFO "TSC2046: Driver init failed with init_TSC2046_obj");
+        printk(KERN_INFO "TSC2046: Driver init failed with init_TSC2046_obj\n");
+        destroy_workqueue(wq);
         return -EINVAL;
     }
     
@@ -300,6 +418,7 @@ static int __init initialization(void){
         destroy_TSC2046_obj(device_obj);
         kset_unregister(TSC2046_kset);
         printk(KERN_INFO "TSC2046: Please pass a valid pin for penirq\n");
+        destroy_workqueue(wq);
         return -EINVAL;
     }
     
@@ -309,15 +428,18 @@ static int __init initialization(void){
         destroy_TSC2046_obj(device_obj);
         kset_unregister(TSC2046_kset);
         printk(KERN_INFO "TS2046: Init Failed, Bad Master port, %dh", spiport);
+        destroy_workqueue(wq);
         return -EINVAL;
     }
     //printk(KERN_INFO "TS2046:pass busnum to master\n");
+    
     spi_dev = spi_alloc_device(master);
     if (!spi_dev) {
         destroy_TSC2046_obj(device_obj);
         kset_unregister(TSC2046_kset);
         put_device(&master->dev);
         printk(KERN_INFO "TSC2046: Init Failed, Bad Device\n");
+        destroy_workqueue(wq);
         return -EINVAL;
     }
     //printk(KERN_INFO "TS2046:passed spi_alloc\n");
@@ -347,10 +469,12 @@ static int __init initialization(void){
         spi_dev_put(spi_dev);
         put_device(&master->dev);
         printk(KERN_INFO "TS2046: Init Failed: Failed to add device\n");
+        destroy_workqueue(wq);
         return status;
     }
     //printk(KERN_INFO "TS2046:pass spi add\n");
     
+    // GPIO and INterrupt
     gpio_request(penirq, "sysfs");
     gpio_direction_input(penirq);
     gpio_set_debounce(penirq, 200);
@@ -363,7 +487,12 @@ static int __init initialization(void){
                         IRQF_TRIGGER_FALLING | IRQF_TRIGGER_RISING,   // Interrupt on rising edge (button press, not release)
                         "penirq_interrupt_handler",    // Used in /proc/interrupts to identify the owner
                         NULL);
-    printk(KERN_INFO "TSC2046: Initializations Done");
+                        
+                        
+    // Work Queue initialization
+    
+    
+    printk(KERN_INFO "TSC2046: Initializations Done\n");
     return status;
 }
 
@@ -373,12 +502,27 @@ static void __exit exiting(void) {
     destroy_TSC2046_obj(device_obj);
     kset_unregister(TSC2046_kset);
     gpio_free(penirq);
-    printk(KERN_INFO "TSC2046: Goodbye");
+    destroy_workqueue(wq);
+    printk(KERN_INFO "TSC2046: Goodbye\n");
     
 }
 
 static irq_handler_t penirq_interrupt_handler(unsigned int irq, void *dev_id, struct pt_regs *regs) {
     printk(KERN_INFO "TSC2046: Trigger for IRQ, state: %d\n", gpio_get_value(penirq));
+    struct screenVals* vals;
+    struct diffVals* diffs;
+    vals = &device_obj->vals;
+    diffs = &device_obj->diffs;
+    if (gpio_get_value(penirq)) { // if it is a 1, it is released;
+        vals->touch = false;
+        diffs->time_down_ns = get_time_ns() - diffs->start_time_ns;
+    } else { // if it is a 0, it is pressed
+        vals->touch = true;
+        diffs->time_down_ns = 0;
+        diffs->start_time_ns = get_time_ns();
+    }
+    INIT_WORK(&main_spi_work_struct, main_spi_work_handler);
+    schedule_work(&work);
     return (irq_handler_t) IRQ_HANDLED;
 }
 
